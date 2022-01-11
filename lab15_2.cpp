be757e8338d5169ec93e95448f16bc8dad0e5b45
Nitchamon <Aonoony@gmail.com>
2022-01-11 21:01:49 +0700

    "lab15_2.cpp completed"

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -19,3 +19,25 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+void showMatrix(const bool b[][N]){
+	for(int i = 0; i<N; i++){
+		for(int j = 0; j<N; j++) cout << b[i][j] << " ";
+		cout << endl; 
+	}
+}
+void inputMatrix(double a[][N]){
+	for(int i = 0; i<N; i++){
+		cout << "Row " << i+1 << ": ";
+		for(int j = 0; j<N; j++) cin >> a[i][j];
+	}
+}
+void findLocalMax(const double a[][N], bool b[][N]){
+	for(int i = 0; i<N; i++){
+		for(int j = 0; j<N; j++){
+			if(a[j][i] >= a[j+1][i] && a[j][i] >= a[j-1][i] && a[j][i] >= a[j][i+1] && a[j][i] >= a[j][i-1]){
+				if(j != 0 && j != N-1 && i !=0 && i != N-1) b[j][i] = 1;
+				else b[j][i] = 0;
+			}else b[j][i] = 0;
+		}
+	}
+}
